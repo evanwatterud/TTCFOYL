0ccc45f12f82ba3dee51c6b87ce824adb3c623aa
Evan Watterud <cod.videos@hotmail.com>
2018-01-25 13:47:12 -0500

    added simple nav

--- App.js
+++ App.js
@@ -1,17 +1,27 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { StackNavigator } from 'react-navigation';
+
+
+class HomeScreen extends React.Component {
+  static navigationOptions = {
+    title: 'Welcome'
+  };
+  render() {
+    return <Text>Hello, Navigation!</Text>;
+  }
+}
+
+const SimpleApp = StackNavigator({
+  Home: { screen: HomeScreen }
+});
 
 export default class App extends React.Component {
   render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-      </View>
-    );
+      return <SimpleApp />;
   }
 }
+
 
 const styles = StyleSheet.create({
   container: {
